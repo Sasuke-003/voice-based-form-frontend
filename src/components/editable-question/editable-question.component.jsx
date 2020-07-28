efe8d164d1d0fffc515beffb79a938e9f6a8b484
kushal-p1699 <kushal.p1699@gmail.com>
2020-07-27 23:47:14 -0700

    card-added-2

--- src/components/editable-question/editable-question.component.jsx
+++ src/components/editable-question/editable-question.component.jsx
@@ -1,11 +1,44 @@
 import React from "react";
 
 import TextField from "@material-ui/core/TextField";
-
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
 import "./editable-question.styles.css";
 
+const useStyles = makeStyles({
+  root: {
+    width: "770px",
+    margin: "auto",
+    paddingBottom: "64px",
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
 const EditableQuestion = ({ data, handleChange }) => {
-  return <div></div>;
+  const classes = useStyles();
+  return (
+    <section className="home-section">
+      <Card className={classes.root}>
+        <CardContent></CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </section>
+  );
 };
 
 export default EditableQuestion;
