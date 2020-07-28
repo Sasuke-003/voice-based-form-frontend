86e624dff2bf2b06fb8ca2a6f18a4f4fe7de148c
kushal-p1699 <kushal.p1699@gmail.com>
2020-07-27 23:41:46 -0700

    card-added

--- src/components/editable-question/editable-question.component.jsx
+++ src/components/editable-question/editable-question.component.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import "./editable-question.styles.css";
 
-import './editable-question.styles.css'
+const useStyles = makeStyles({
+  root: {
+    width: "770px",
+    margin: "auto",
+    paddingBottom: "64px",
+  },
+});
 
 const EditableQuestion = ({ qData }) => {
-
-
-    return (
-
-        <div>
-
-            
-        </div>
-
-    );
-
+  const classes = useStyles();
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardContent></CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </div>
+  );
 };
 
 export default EditableQuestion;
