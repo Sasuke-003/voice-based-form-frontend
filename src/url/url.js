88b0dc5c6ba576691abbf56909fb9358769c84ae
NEURON <50287102+neuron909@users.noreply.github.com>
2020-07-30 15:25:33 +0530

    Merge: URL

--- src/url/url.js
+++ src/url/url.js
@@ -1,4 +1,7 @@
-import axios from 'axios'
+import axios from 'axios' ;
+import moment from 'moment'
+import { store } from '../redux/store' ;
+import { setCurrentUser } from '../redux/user/user.actions' ;
 
 export const URL = {
 
@@ -9,7 +12,10 @@ export const URL = {
     formCreate : '/form/template/new',
     formDetail : '/form/template/detail/',
     formAnswer : '/form/ans/new',
-
+    
+    /////////////////////////////////////
+    refreshToken : '/auth/refresh-token',
+    accessToken  : '/auth/access-token',
 }
 
 
@@ -17,18 +23,47 @@ export const req = {
 
     user: {
 
-        signUp  : async (data) => { return await axios.post( URL.signUp, data ) },
-        signIn  : async (data) => { return await axios.post( URL.signIn, data ) },
-        signOut : async () => { return await await axios.get( URL.signOut ) }
-
+        signUp  : async (body) => { return await axios.post( URL.signUp, body ) },
+        signIn  : async (body) => {
+            localStorage.setItem( "nextRefreshTime" , moment().add(1,'days') );
+            const resData = await axios.post( URL.signIn, body );
+            axios.defaults.headers.common['Authorization'] = resData.AccessToken;
+            return resData;
+        },
+        signOut : async ( ) => { 
+            axios.get( URL.signOut ); 
+            localStorage.clear();
+            // Clears all Cookie  ( From : https://stackoverflow.com/questions/179355/clearing-all-cookies-with-javascript )
+            document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+            store.dispatch(setCurrentUser(null));
+        }
     },
 
     form: {
 
-        create : async ( data ) => { return await axios.post( URL.formCreate, data ) },
-        detail : async ( data ) => { return await axios.get( URL.formDetail+data ) },
+        create : async ( body  ) => { return await axios.post( URL.formCreate, body ) },
+        detail : async ( param ) => { console.log(URL.formDetail+param); return await axios.get( URL.formDetail+param ) },
         answer : async ( data ) => { return await axios.post( URL.formAnswer, data ) },
 
     },
-    
+    auth : {
+        refreshToken : async () => {
+            if ( moment(localStorage.getItem( "nextRefreshTime" )) < moment() ) {
+                localStorage.setItem( "nextRefreshTime" , moment().add(1,'days') )
+                const res = await axios.get( URL.refreshToken );
+                axios.defaults.headers.common['Authorization'] = res.AccessToken ;
+            }
+        },
+        accessToken : async ( failedReq ) => {
+            const res = await axios.get( URL.accessToken );
+            axios.defaults.headers.common['Authorization'] = res.AccessToken ;
 
+            if ( failedReq ) {
+                failedReq.headers[ 'Authorization']  = res.AccessToken ;
+                failedReq.data = JSON.parse( `${failedReq.data}` ) ;
+                return await axios.request( failedReq ) ; 
+            }
+            return res;
+        },
+    }
+}
